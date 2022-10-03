47f95cb7732ad01df3c693f6fe092521e9ac82a7
AnnieHow <annie.howard.e@gmail.com>
2022-10-03 15:08:46 -0400

    Cleaning up dod.topo and dod.met

--- R/met.R
+++ R/met.R
@@ -1,18 +1,18 @@
 #' Download meteorological data
 #'
 #' This function downloads topographic data using
-#' [oce::download.met].
+#' [oce::download.met()].
 #'
-#' @param ... additional arguments passed to
-#' [oce::download.met].
+#' @param ... arguments passed to
+#' [oce::download.met()].
 #'
 #' @importFrom oce download.met
 #'
-#'
-#' @return fix me
+#' @return String indicating the full pathname to the downloaded file.
 #'
 #' @export
 
-dod.met <- function(...) {
-  oce::download.met(...)
+dod.met <- function(...)
+{
+    oce::download.met(...)
 }
--- R/topo.R
+++ R/topo.R
@@ -1,33 +1,19 @@
 #' Download topographic data
 #'
 #' This function downloads topographic data using
-#' [oce::download.topo].
+#' [oce::download.topo()].
 #'
-#' @param west,east numeric values for the limits
-#' of the data-selection box, in degrees. These
-#' are converted to the -180 to 180 degree notation,
-#' if needed. Then, west is rounded down to the nearest
-#' 1/100th degree, and east is rounded up to the the
-#' nearest 1/100th degree. The results of these
-#' operations are used in constructing the query for
-#' the NOAA data server.
-#'
-#' @param south,north latitude limits, treated in
-#' a way that corresponds to the longitude limits.
-#'
-#' @param ... additional arguments passed to
-#' [oce::download.topo].
+#' @param ... arguments passed to
+#' [oce::download.topo()].
 #'
 #' @importFrom oce download.topo
-#' @importFrom oce read.topo
 #'
-#' @return An oce topo object.
+#' @return String indicating the full pathname to the downloaded file.
 #'
 #' @export
 
-dod.topo <- function(west=-66, east=-60, south=43, north=47, ...) {
-  topoFile <- oce::download.topo(west=west, east=east, north=north, south=south,...)
-  topo <- read.topo(topoFile)
-  return(topo)
+dod.topo <- function(...)
+{
+  oce::download.topo(...)
 }
 
