2c0dff7a5dc3ad8f8112e0241f29da1ade837468
AnnieHow <annie.howard.e@gmail.com>
2022-10-06 12:53:43 -0400

    Edits to dod.buoy and documenting dodDebug

--- R/buoy.R
+++ R/buoy.R
@@ -6,9 +6,8 @@
 #' @param program argument specifying the desired oceanographic
 #' program to download buoy data from. Options include `MEDS`, etc.
 #'
-#' @param location
-#'
-#' @param ID for `MEDS`...
+#' @param ID For `MEDS` this argument is either a location name
+#' or ID number.
 #'
 #' @param destdir character value indicating the directory
 #' in which to store downloaded files.
@@ -35,7 +34,7 @@ dod.buoy <- function(program, ID=NULL, destdir=".", debug=0)
     if (is.null(ID))
         stop("Must provide an ID argument")
     if (program == "MEDS") {
-        loc <- list("East Scotian Slope"="44137","Banquereau Bank"="44139")
+        loc <- list("East Scotian Slope"="44137","Banquereau Bank"="44139", "Halifax Harbour"="44258","Halifax"="44172","Halifax DISCUS TriAx"= "44299","Tail of the Bank"= "44140","Laurentian Fan"="44141","Port Hope"="45135", "Prince Edward Point"= "45135","Minas Basin"= "MEDS027")
         dodDebug(debug, "Initial ID=", ID,"\n")
         if (ID %in% names(loc))
             ID <- loc[[ID]]
@@ -43,7 +42,7 @@ dod.buoy <- function(program, ID=NULL, destdir=".", debug=0)
         server <- "https://www.meds-sdmm.dfo-mpo.gc.ca/alphapro/wave/waveshare/csvData"
         url <- paste0(server, "/c", ID, "_csv.zip")
         zipfile <- paste0("c", ID, "_csv.zip")
-        message(url)
+        dodDebug(debug, url)
         download.file(url, zipfile)
         unzip(zipfile)
         unlink(zipfile)
--- R/debug.R
+++ R/debug.R
@@ -1,3 +1,15 @@
+#' Debug dod package fucntions
+#'
+#' This function is used throughout the `dod` package to catch
+#' bugs and provide assistance to users.
+#'
+#' @param debug integer value indicating level of debugging.
+#' If this is less than 1, no debugging is done. Otherwise,
+#' some functions will print debugging information.
+#'
+#' @param ... FILL IN not sure if this pulling from another
+#' package or what.
+#'
 #' @export
 dodDebug <- function(debug=0, ...)
 {
