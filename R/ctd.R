cc044a269ffb3881039e5426cf2a818e3bf65c3c
AnnieHow <annie.howard.e@gmail.com>
2022-11-04 15:40:26 -0400

    Edits to dod.ctd

--- R/ctd.R
+++ R/ctd.R
@@ -2,31 +2,32 @@
 #'
 #' This function downloads CTD data from various programs.
 #'
-#' |                    **Project**                   | **Program** |
-#' |                                            :---- |       :---- |
-#' |Bedford Basin Mooring Project                     |      `BBMP` |
-#' |Bermuda Atlantic Time Series                      |      `BATS` |
-#' |Global Temperature and Salinity Profile Programme |     `GTSPP` |
+#' |                    **Project**                   | **Program** |**Function Called**|
+#' |                                            :---- |       :---- |              :----|
+#' |Bedford Basin Mooring Project                     |      `BBMP` |   [dod.ctd.bbmp()]|
+#' |Bermuda Atlantic Time Series                      |      `BATS` |   [dod.ctd.bats()]|
+#' |Global Temperature and Salinity Profile Programme |     `GTSPP` |  [dod.ctd.gtspp()]|
 #'
 #' @param program character value naming the program (one of listthem).
 #'
 #' @param ... extra arguments passed to [dod.ctd.bats()] or one of the
 #' other functions.
 #'
-#' If `program` is `"BATS"`, call [dod.ctd.bats()].
-#' If `program` is `"BBMP"`, call [dod.ctd.bbmp()].
-#' If `program` is `"GTSPP"`, call [dod.ctd.gtspp()].
-#'
 #' @export
 
 dod.ctd <- function(program=NULL, ...)
 {
+    allowed <- c("BATS", "BBMP", "GTSPP")
     if (is.null(program))
-        stop("must give 'program'; try \"BATS\", \"BBMP\",\"GTSPP\", ...")
+        stop("must give 'program'; try \"",
+             paste(allowed, collapse='" "'), "\"")
     if (program == "BATS")
         dod.ctd.bats(...)
     else if (program == "BBMP")
         dod.ctd.bbmp(...)
     else if (program == "GTSPP")
         dod.ctd.gtspp(...)
+    else
+        stop("'program' must be one of: \"",
+             paste(allowed, collapse='" "'), "\"")
 }
