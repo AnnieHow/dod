c4b0625efe79c8735468ecb75739ab5107534d30
Annie Howard <anniehoward@Annies-MacBook-Pro.local>
2022-11-17 13:16:29 -0500

    Pushing test suites

--- tests/testthat/test_ctd.R
+++ tests/testthat/test_ctd.R
@@ -5,4 +5,8 @@ test_that("ctd downloads", {
     expect_error(dod.ctd("BBMP"), "must give 'year'")
     expect_true(is.data.frame(dod.ctd("BBMP", index=TRUE, year=2022)))
     expect_equal(dod.ctd("BBMP", year="2022", ID="D22667034.ODF"), "./D22667034.ODF")
+    expect_error(dod.ctd("BATS"), "Must provide an ID number greater than 10000")
+    expect_error(dod.ctd("BATS"), "Must provide an ID number greater than 10000")
+    expect_error(dod.ctd("GTSPP"), "must give an ID")
+    expect_equal(dod.ctd("BATS", year="2012", ID="10001"), "./b10001_ctd.txt")
 })
