74b941b143db1dbd474faa56ad5fd3c49d8b914f
Jaimie Harbin <jm810136@dal.ca>
2022-12-01 12:06:41 -0400

    add AllClass

---
+++ R/AllClass.R
@@ -0,0 +1,13 @@
+# vim:textwidth=128:expandtab:shiftwidth=4:softtabstop=4
+
+#' A package for downloading publically available data.
+#'
+#' The `dod` package provides tools for downloading Advanced
+#' Microwave Scanning Radiometer (AMSR), buoy, coastline, CTD (BATS,
+#' BBMP, and GTSPP), meteorological, and topographic data.
+#' @importFrom methods new
+#' @name dod-package
+#' @docType package
+NULL
+
+
