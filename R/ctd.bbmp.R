391aa29fdb2301180da4565f64af1d8cf77ce10d
dankelley <kelley.dan@gmail.com>
2022-12-22 12:22:10 -0400

    dod.ctd.bbmp() drops the 'read' parameter
    I'm in the middle of other changes and am just pushing these to have a
    clean commit history.  I'm not even sure the package will build/test
    with this exact commit.  Later today, I'll alter ctd.bats.R and ensure
    that any tests are also updated and then I'll push the final (then
    working) version.
    
    All of this is pursuant to a Z with JH earlier today.

--- R/ctd.bbmp.R
+++ R/ctd.bbmp.R
@@ -1,6 +1,10 @@
-#' Download CTD data from the Bedford Basin Mooring Project (BBMP)
+#' Download CTD data from the Bedford Basin Mooring Project
 #'
-#' This function downloads CTD data from BBMP.
+#' [dod.ctd.bbmp] can retrieve both index files and data files
+#' from the Bedford Basin Mooring Project (BBMP). Since the naming
+#' convention of the data files may alter from year to year, it is
+#' important to start by downloading an index first, to ascertain
+#' the name of a file of interest; see \sQuote{Examples}.
 #'
 #' @param year a character value specifying the year of interest.
 #'
@@ -13,34 +17,38 @@
 #' @param file character value giving the name to be used for
 #' the downloaded file.
 #'
-#' @param read a boolean indicated weather or not the downloaded
-#'
 #' @template destdirTemplate
 #'
 #' @template debugTemplate
 #'
 #' @importFrom utils read.csv
-#' @importFrom oce read.oce
+## @importFrom oce read.oce
 #'
-#' @return If `read` is FALSE, a downloaded file is returned. If `read`
-#' is TRUE and `index` is TRUE a data frame is returned. If `read` is TRUE
-#' and `index` is FALSE an oce object is returned.
+#' @return [dod.ctd.bbmp] returns a character value naming the file that
+#' was retrieved. This may either be an index file or a data file;
+#' see \sQuote{Examples} for a typical workflow in which an index is
+#' retrieved and then used as a guide to the name of a data file of
+#' interest.
 #'
 #' @examples
 #'\dontrun{
 #' # Download the first file of year 2022.
 #' library(dod)
+#' # Download the index
+#' indexFile <- dod.ctd("BBMP", year=2022, index=TRUE, file="index_bbmp_2022")
+#' # Read the index (altering this call if the file format changes)
+#' index <- read.csv(indexFile, header=FALSE, col.names=c("file","time"), skip=3)
+#' # Download the first file in the index
+#' ctdFile <- dod.ctd("BBMP", year=2022, ID=index$file[1], file="bbmp_2022_001")
+#' # Use oce to read, summarize and plot the data.
 #' library(oce)
-#' destdir <- "~/data/ctd"
-#' i <- dod.ctd("BBMP", year=2022, index=TRUE, file="index", read=TRUE)
-#' f <- dod.ctd("BBMP", year=2022, ID=i$file[1], destdir=destdir, file="bbmp", read=TRUE)
-#' ctd <- read.ctd(f)
+#' ctd <- read.ctd(ctdFile)
+#' summary(ctd)
 #' plot(ctd)
 #'}
 #'
 #' @export
-
-dod.ctd.bbmp <- function(year, ID=NULL, index=FALSE, file=NULL, destdir=".", debug=0, read=FALSE)
+dod.ctd.bbmp <- function(year, ID=NULL, index=FALSE, file=NULL, destdir=".", debug=0)
 {
     server <- "ftp://ftp.dfo-mpo.gc.ca/BIOWebMaster/BBMP/ODF"
     if (missing(year))
@@ -60,9 +68,10 @@ dod.ctd.bbmp <- function(year, ID=NULL, index=FALSE, file=NULL, destdir=".", deb
         dodDebug(debug, oce::vectorShow(file))
         url <- paste0(server, "/", file)
         file <- paste0(destdir,"/",file)
-        if (read) {
-        return(read.csv(file, header=FALSE, skip=3, col.names=c("file", "time")))
-        }
+        return(file)
+        #if (read) {
+        #return(read.csv(file, header=FALSE, skip=3, col.names=c("file", "time")))
+        #}
     } else {
         if (is.null(ID))
             stop("Must provide an ID from the index")
@@ -76,9 +85,10 @@ dod.ctd.bbmp <- function(year, ID=NULL, index=FALSE, file=NULL, destdir=".", deb
         dodDebug(debug, oce::vectorShow(url))
         dodDebug(debug, oce::vectorShow(ID))
         file <- dod.download(url=url, file=ifelse(grepl("ODF", file) == FALSE, paste0(file, ".ODF"), file), destdir=destdir, silent=TRUE,debug=debug)
-        if (read == TRUE) {
-            t <- oce::read.oce(file)
-            return(t)
-        }
+        return(file)
+        #if (read == TRUE) {
+        #    t <- oce::read.oce(file)
+        #    return(t)
+        #}
     }
 }
--- tests/testthat/test_ctd.R
+++ tests/testthat/test_ctd.R
@@ -4,7 +4,7 @@ library(testthat)
 test_that("ctd downloads", {
     expect_error(dod.ctd("invalid"), "'program' must be")
     expect_error(dod.ctd("BBMP"), "must give 'year'")
-    expect_true(is.data.frame(dod.ctd(program="BBMP", index=TRUE, year=2022, read=TRUE)))
+    expect_equal("./2022667ODFSUMMARY.tsv", dod.ctd(program="BBMP", index=TRUE, year=2022))
     expect_silent(dod.ctd(program="BBMP", year="2022", ID="D22667034.ODF"))
     expect_error(dod.ctd("BATS"), "Must provide an ID number greater than 10000")
     expect_error(dod.ctd("BATS"), "Must provide an ID number greater than 10000")
