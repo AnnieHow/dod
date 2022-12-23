7f0fb677c3c0adfec8e00122cc2364a2e16d69da
dankelley <kelley.dan@gmail.com>
2022-12-23 06:40:52 -0400

    indent code, and reword a few things for clarity
    I made a video explaining this work, and it will be at
    https://www.dropbox.com/s/t2elf7mav9danb6/why_indent_R.mp4?dl=0
    for a week or so.

--- R/ctd.gtspp.R
+++ R/ctd.gtspp.R
@@ -24,15 +24,14 @@
 #' @template debugTemplate
 #'
 #' @importFrom utils read.csv
-#' @importFrom oce read.odf
 #' @importFrom ncdf4 nc_open
 #'
-#' @return 1) If `index` = TRUE and `read`=FALSE a gz file is returned.
-#' 2) If `index`=TRUE and `read` = TRUE, a data frame with a list of
-#' NetCDF files names is returned. 3) If `index`=FALSE and
-#' `read` = FALSE, a netcdf file of the specified index is returned.
-#' 4) If `index` = FALSE and `read` = TRUE, a read netcdf file is
-#' turned.
+#' @return 1) If `index` is TRUE and `read` is FALSE, then a gz file is returned.
+#' 2) If both `index` and `read` are TRUE, then a data frame with a list of
+#' NetCDF files names is returned. 3) If both `index` and
+#' `read` are FALSE, then a netcdf file of the specified index is returned.
+#' 4) Finally, if `index` is FALSE and `read` is TRUE, the contents of the data file are
+#' returned as an oce `ctd` object.
 #'
 #' @family functions that download CTD data
 #'
@@ -40,67 +39,49 @@
 dod.ctd.gtspp <- function(ID=NULL, file=NULL, destdir=".", debug=0, index=TRUE, read=FALSE, nc=NULL)
 {
     server <- "https://www.ncei.noaa.gov/data/oceans/gtspp/bestcopy/"
-        if (is.null(ID)) {
-            stop("must give an ID. See help pages for the correct format")
-        }
-        server <- paste0(server)
-        if (grepl("at", ID)) {
+    if (is.null(ID))
+        stop("must give an ID. See help pages for the correct format")
+    if (grepl("at", ID)) {
         dodDebug(debug, "Ocean has been identified as atlantic\n")
         server <- paste0(server, "atlantic")
-
-        } else if (grepl("pa", ID)) {
+    } else if (grepl("pa", ID)) {
         dodDebug(debug, "Ocean has been identified as pacific\n")
         server <- paste0(server, "pacific")
-        } else if (grepl("in", ID)) {
+    } else if (grepl("in", ID)) {
         dodDebug(debug, "Ocean has been identified as indian\n")
         server <- paste0(server, "indian")
+    }
+    year <- substr(ID, start = 3, stop = 6)
+    dodDebug(debug, oce::vectorShow(year))
+    month <- substr(ID, start = 7, stop = 8)
+    dodDebug(debug, oce::vectorShow(month))
+    server <- paste0(server, "/", year, "/", month)
+    dodDebug(debug, oce::vectorShow(server))
+    dodDebug(debug, oce::vectorShow(destdir))
+    if (index) {
+        if (!(is.null(nc)))
+            message("nc argument being ignored. Only need to specify when index=FALSE")
+        file <- if (is.null(file)) paste0(ID,".gz") else paste0(file, ".gz")
+        d <- dod.download(url=server, file=file, destdir=destdir, silent=TRUE,debug=debug)
+        if (read) {
+            lines <- readLines(d)
+            lines <- lines[-(1:11)]
+            lines <- gsub(".*href=\"","",lines)
+            lines <- gsub("\".*","",lines)
+            df <- data.frame(lines)
+            names(df) <- "index"
+            return(df)
         }
-        year <- substr(ID, start = 3, stop = 6)
-        dodDebug(debug, oce::vectorShow(year))
-        month <- substr(ID, start = 7, stop = 8)
-        dodDebug(debug, oce::vectorShow(month))
-        server <- paste0(server, "/", year, "/", month)
-
-        dodDebug(debug, oce::vectorShow(server))
-        dodDebug(debug, oce::vectorShow(destdir))
-        if (index) {
-            if (!(is.null(nc))) {
-                message("nc argument being ignored. Only need to specify when index=FALSE")
-            }
-        if (is.null(file)) {
-            file <- paste0(ID,".gz")
-        } else {
-            file=paste0(file, ".gz")
+    } else {
+        dodDebug(debug, oce::vectorShow(nc))
+        server <- paste0(server,"/", nc)
+        file  <- if (is.null(file)) paste0(nc) else paste0(file, ".nc")
+        d <- dod.download(url=server, file=file, destdir=destdir, silent=TRUE,debug=debug)
+        if (read) {
+            dodDebug(debug, oce::vectorShow(file))
+            netcdf <- ncdf4::nc_open(file)
+            return(netcdf)
         }
-            d <- dod.download(url=server, file=file, destdir=destdir, silent=TRUE,debug=debug)
-            if (read) {
-                lines <- readLines(d)
-                lines <- lines[-(1:11)]
-                lines <- gsub(".*href=\"","",lines)
-                lines <- gsub("\".*","",lines)
-                df <- data.frame(lines)
-                names(df) <- "index"
-                return(df)
-            }
-        } else {
-            dodDebug(debug, oce::vectorShow(nc))
-            server <- paste0(server,"/", nc)
-            if (is.null(file)) {
-                file <- paste0(nc)
-            } else {
-                file=paste0(file, ".nc")
-            }
-            d <- dod.download(url=server, file=file, destdir=destdir, silent=TRUE,debug=debug)
-            if (read) {
-                dodDebug(debug, oce::vectorShow(file))
-                netcdf <- ncdf4::nc_open(file)
-                return(netcdf)
-            }
-
-
-        }
-
+    }
 }
-
-
 
