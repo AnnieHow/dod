79a084eacd156952e862dd27b2b7f910a1dc748e
dankelley <kelley.dan@gmail.com>
2023-07-27 08:21:59 -0300

    recode dod.amsr().  type="3day" now working

--- NEWS.md
+++ NEWS.md
@@ -1,8 +1,9 @@
 # dod 0.1.3
 
 * `dod.amsr()` is now local, as opposed to a call to `oce::download.amsr()`.
-  Also, it queries the new URL pattern and file type (which used to be `.gz` but
-  now is `.nc`).  See https://github.com/dankelley/oce/issues/2124.
+  It is also changed in major ways, because the data provider has altered
+  both the directory structure and the data format.  For more, see
+  https://github.com/dankelley/oce/issues/2124.
 
 # dod 0.1.2
 
--- R/amsr.R
+++ R/amsr.R
@@ -1,34 +1,59 @@
+# vim:textwidth=200:expandtab:shiftwidth=4:softtabstop=4
+
 #' Download Advanced Microwave Scanning Radiometer data
 #'
 #' This function downloads AMSR data.
 #'
-#' @param year integer giving year (defaults to value 3 days in past).
-#'
-#' @param month integer giving month (defaults to value 3 days in past).
-#'
-#' @param day integer giving day (defaults to value 3 days in past).
+#' @param year,month,day integer values indicating the desired observation time.
+#' All three are required for `server` values of `"3day"` and `"daily"`,
+#' but day is not required (and are ignored) for the other two choices of `server`.
+#' If these values are not provided, the focus is 3 days prior to the present day.
 #'
 #' @param destdir character giving destination directory (defaults to `"."`, the present
 #' directory).
 #'
 #' @param age integer giving the number of days old a local file has to be, for it to
-#' be regarded as in need of replacement.
+#' be regarded as in need of replacement. This defaults to a year.
 #'
-#' @param server character value indicating where to get the data.  This may be `"new"` (the default)
-#' for a scheme that worked in year 2023, `"old"` for a scheme that worked a few years 
+#' @param server character value indicating the base server location. The
+#' default value ought to be used unless the data provider changes their
+#' web scheme ... but in that case, it is hoped that users will contact
+#' the developers so that the package can be updated.
+#'
+#' @param type character value indicating where to get the data.  This may be
+#' `"3day"` (the default), for a composite covering 3 days of observation, which
+#' removes most viewing-path and cloud blanks, `"daily"` for a daily reading,
+#' `"weekly"` for a composite covering a week, or `"monthly"` for a composite
+#' covering a month.  In the `"daily"` case, the data arrays are 3D, with the
+#' third dimension representing ascending and descending traces, but in all the
+#' other cases, the arrays are 2D.
 #'
 #' @param debug integer giving debugging level (defaults to 0, to make the function work
 #' without much output).
 #'
 #' @return character value indicating the full pathname to the downloaded file.
 #'
+#' @section Historical note:
+#' In July 2023, the author noticed major changes in the organization of
+#' the remove server, and also of the data files provided. This required
+#' major changes to [dod.amsr()], especially in regard to the `server`
+#' argument and the newly-added `type` argument. Users are asked to be
+#' on the lookout for problems.  Also note that [oce::read.amsr()] will
+#' need adjustment to read the new file format.
+#'
 #' @export
-
-dod.amsr <- function(year, month, day, destdir=".", age=14, server="new", debug=0)
+#'
+#' @author Dan Kelley
+dod.amsr <- function(year, month, day, destdir=".", age=365,
+    server="https://data.remss.com/amsr2/ocean/L3/v08.2",
+    type="3day", debug=0)
 {
+    message("type=\"", type, "\"")
+    dodDebug(debug, "dod.amsr(type=\"", type, "\", ...) {\n", sep="")
+    if (!type %in% c("3day", "daily", "weekly", "monthly"))
+        stop("type value \"", type, "\" is not permitted.  Please see documentation")
     # If year, month, day not given, default to 3 days ago.
     recent <- as.POSIXlt(Sys.Date() - 3)
-    dodDebug(debug, "recent=", format(recent), "\n")
     if (missing(year))
         year <- 1900 + recent$year
     if (missing(month))
@@ -38,21 +63,28 @@ dod.amsr <- function(year, month, day, destdir=".", age=14, server="new", debug=
     year <- as.integer(year)
     month <- as.integer(month)
     day <- as.integer(day)
-    dodDebug(debug, sprintf("year=%d, month=%d, day=%d\n", year, month, day))
-    # Special cases for 'new' and 'old'; otherwise, will
-    # use as-is.
-    serverOrig <- server
-    if (identical(server, "new")) {
-        url <- sprintf("https://data.remss.com/amsr2/ocean/L3/v08.2/daily/%d/RSS_AMSR2_ocean_L3_daily_%d-%02d-%02d_v08.2.nc", year, year, month, day)
-        file <- gsub(".*/", "", url)
-        #dodDebug(debug, "will try url \"", url, "\"\n", sep="")
-        #dodDebug(debug, "will try file \"", file, "\"\n", sep="")
-        #message("https://data.remss.com/amsr2/ocean/L3/v08.2/daily/2023/RSS_AMSR2_ocean_L3_daily_2023-07-23_v08.2.nc")
-        #message(url)
-        filename <- dod.download(url, destdir=destdir, file=file, age=age, debug=debug-1)
-        return(filename)
-    } else if (identical(server, "old")) {
-        stop("FIX FOR OLD")
-        server <- "http://data.remss.com/amsr2/bmaps_v08"
+    dodDebug(debug, sprintf("year=%d, month=%d, day=%d, type=%s\n", year, month, day, type))
+    if (identical(type, "3day")) {
+        #?https://data.remss.com/amsr2/ocean/L3/v08.2/3day/2023/RSS_AMSR2_ocean_L3_3day_2023-07-24_v08.2.nc
+        # https://data.remss.com/amsr2/ocean/L3/v08.2/3day/2023/RSS_AMSR2_ocean_L3_3day_2023-07-24_v08.2.nc
+        # ^                                           ^    ^                       ^    ^    ^  ^
+        # server                                      type year                  type year month day
+        url <- sprintf("%s/%s/%d/RSS_AMSR2_ocean_L3_%s_%04d-%02d-%02d_v08.2.nc",
+            server, type, year, type, year, month, day)
+        # https://data.remss.com/amsr2/ocean/L3/v08.2/3day/2023/RSS_AMSR2_ocean_L3_3day-2023-07-24_v08.2.nc
+    } else if (identical(type, "daily")) {
+        stop("FIXME daily")
+    } else if (identical(type, "weekly")) {
+        stop("FIXME daily")
+    } else if (identical(type, "monthley")) {
+        stop("FIXME daily")
+    } else {
+        stop("unrecognized '", server, "' value; see documentation.")
     }
+    file <- gsub(".*/", "", url)
+    dodDebug(debug, "url=\"", url, "\"\n", sep="")
+    dodDebug(debug, "file=\"", file, "\"\n", sep="")
+    rval <- dod.download(url, destdir=destdir, file=file, age=age, debug=debug-1, silent=debug==0)
+    dodDebug(debug, "} # dod.amsr\n", sep="")
+    rval
 }
