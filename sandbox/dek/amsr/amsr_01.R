ce1677043a82b1cb2da67ef434e4d43fcb545ed7
dankelley <kelley.dan@gmail.com>
2023-07-27 11:57:59 -0300

    amsr test to sandbox

--- sandbox/dek/amsr/amsr_01.R
+++ sandbox/dek/amsr/amsr_01.R
@@ -1,5 +1,11 @@
 library(dod)
 system("rm -rf RSS*.nc")
+# Next will download 4 files
+dod.amsr() # defaults to 3day
+dod.amsr(type="daily")
+dod.amsr(type="weekly")
+dod.amsr(type="monthly")
+# Next won't download anything (cached)
 dod.amsr() # defaults to 3day
 dod.amsr(type="daily")
 dod.amsr(type="weekly")
